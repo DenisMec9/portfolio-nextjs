054118f96571cfd4891405edf8cf6747eb10a7ee
DenisMec9 <denisfilho2@gmail.com>
2025-05-28 13:44:17 -0300

    ok

--- pages/_app.js
+++ pages/_app.js
@@ -1,5 +1,5 @@
 import '../styles/globals.css';
-import Navbar from 'pages/components/Navbar';
+import Navbar from '../components/Navbar';
 
 export default function App({ Component, pageProps }) {
   return (
