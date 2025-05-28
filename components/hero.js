e39ad81fe6d507492be234e17dfb3fda46b8014a
DenisMec9 <denisfilho2@gmail.com>
2025-05-27 22:27:36 -0300

    alterando os arquivos

---
+++ components/hero.js
@@ -0,0 +1,13 @@
+// components/Hero.js
+export default function Hero() {
+  return (
+    <section className="hero">
+      <h1>Olá, eu sou <span className="nome">Denis</span></h1>
+      <h2>Desenvolvedor Front-End</h2>
+      <p>
+        Criando soluções modernas com React, Next.js e foco total em performance, acessibilidade e boas práticas de código.
+      </p>
+      <a className="botao" href="/projetos">Veja meus projetos</a>
+    </section>
+  );
+}
